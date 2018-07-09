c4b1e54989205a1a457a543c4d36054a31280fae
Francesco Lamonica <lamonica@netresults.it>
2018-07-09 12:31:36 +0000

    Restructured dbhandler repo
    git-svn-id: svn://192.168.23.200/Util/dbhandler_class/trunk@3 37bdffd9-94ab-4ebb-ab70-8754b17d8a01

---
+++ src/nrdbhandler.cpp
@@ -0,0 +1,152 @@
+/********************************************************************************
+ *   Copyright (C) 2010-2018 by NetResults S.r.l. ( http://www.netresults.it )  *
+ *   Author(s):                                                                 *
+ *              Francesco Lamonica      <f.lamonica@netresults.it>              *
+ ********************************************************************************/
+
+#include "nrdbhandler.h"
+
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QSqlRecord>
+#include <QSqlResult>
+#include <QDebug>
+#include <QVariant>
+
+#include <UniqLogger.h>
+
+//included for debug statements in ctor
+#include <QCoreApplication>
+#include <QStringList>
+
+
+NrBaseDbHandler::NrBaseDbHandler(const DbhConfig &i_dbconf, QObject *parent)
+    : QObject(parent)
+    , m_DbConf(i_dbconf)
+{
+
+    UniqLogger *ul = UniqLogger::instance(m_DbConf.logId);
+
+    m_logger = ul->createFileLogger("DB", m_DbConf.logFilename, m_DbConf.logwriterConfig);
+    m_logger->setVerbosityAcceptedLevel(m_DbConf.logLevel);
+
+    //check plugins path
+    foreach (QString path, QCoreApplication::libraryPaths())
+        *m_logger << UNQL::LOG_INFO << "library path: " << path << UNQL::eom;
+    foreach (QString plugin, QSqlDatabase::drivers())
+        *m_logger << UNQL::LOG_INFO << "library path: " << plugin << UNQL::eom;
+
+    _M_db = QSqlDatabase::addDatabase(m_DbConf.dbType, m_DbConf.dbConnectionName);
+
+    _M_db.setHostName(m_DbConf.dbHost);
+    _M_db.setPort(m_DbConf.dbPort);
+    _M_db.setDatabaseName(m_DbConf.dbName);
+    _M_db.setUserName(m_DbConf.dbUser);
+    _M_db.setPassword(m_DbConf.dbPass);
+}
+
+
+
+/*!
+  \brief opens a connection towards the already configured Database
+  \return true if the DB is opened successfully, false otherwise
+  */
+bool
+NrBaseDbHandler::openDbConn()
+{
+    bool ok = true;
+    /* qDebug() << "OpenDbConn() -- db is open (" << _M_db.isOpen() <<") -- db is valid ("
+            << _M_db.isValid() <<") -- db is openError ("<< _M_db.isOpenError() <<")"; */
+
+    if ( !_M_db.isOpen() || !_M_db.isValid() ) {
+        if (_M_db.isOpen()) {
+            _M_db.close();
+            *m_logger << UNQL::LOG_CRITICAL << "Database was INVALID but OPEN! -> Closing" << UNQL::eom;
+        }
+        ok = _M_db.open();
+        if (!ok) {
+            *m_logger << UNQL::LOG_CRITICAL << "------ DB ERROR: " <<  _M_db.lastError().text() << UNQL::eom;
+        } else {
+            QSqlQuery q("set timezone to '" + m_DbConf.dbTimeZone + "'", _M_db);
+            Q_UNUSED(q);
+        }
+        //TODO: what if the connection fails???
+    }
+    return ok;
+}
+
+
+
+/*!
+  \brief Closes the connection that the QSqlDatabase may have opened
+  */
+void
+NrBaseDbHandler::closeDbConn()
+{
+    /* qDebug() << "closeDbConn() -- db is open (" << _M_db.isOpen() <<") -- db is valid ("
+            << _M_db.isValid() <<") -- db is openError ("<< _M_db.isOpenError() <<")"; */
+
+    if ( !_M_db.isOpen() ) {        
+        *m_logger << UNQL::LOG_CRITICAL << "Database was NOT OPEN! -> Closing anyway" << UNQL::eom;
+    }
+
+    _M_db.close();
+}
+
+
+
+/*!
+  \brief a commodity function to call a prepared query and log the results
+  \param query the query to be executed;
+  */
+bool
+NrBaseDbHandler::executeQuery(QSqlQuery &query)
+{
+    bool ok = query.exec();
+    if(!ok) {        
+        *m_logger << UNQL::LOG_CRITICAL << "QUERY ERROR - got error: " << query.lastError().text() << " for query (" << query.lastQuery() << ")" << UNQL::eom;
+    }
+
+    return ok;
+}
+
+
+
+/*!
+    \brief a simple test method to check whether the DB is reachable and user can login
+    \return a boolean that is the return value of the openDbConn() method
+  */
+bool
+NrBaseDbHandler::testConnection()
+{
+    bool b = false;
+
+    b = this->openDbConn();
+    this->closeDbConn();
+
+    return b;
+}
+
+
+
+void
+NrBaseDbHandler::rollbackAndClose(const QString &notice)
+{
+    *m_logger << UNQL::LOG_CRITICAL << "Query" << notice << "failed... rolling back transaction" << UNQL::EOM;
+    _M_db.rollback();
+    closeDbConn();
+}
+
+
+
+void
+NrBaseDbHandler::commitAndClose(const QString &notice)
+{
+    bool dbact = _M_db.commit();
+    if (!dbact) {
+        *m_logger << UNQL::LOG_CRITICAL << "Transaction " << notice << " commit failed" << UNQL::EOM;
+    }
+
+    closeDbConn();
+}
+
---
+++ src/nrdbhandler.h
@@ -0,0 +1,84 @@
+/********************************************************************************
+ *   Copyright (C) 2010-2018 by NetResults S.r.l. ( http://www.netresults.it )  *
+ *   Author(s):                                                                 *
+ *              Francesco Lamonica      <f.lamonica@netresults.it>              *
+ ********************************************************************************/
+
+#ifndef _NR_BASE_DBHANDLER_H_
+#define _NR_BASE_DBHANDLER_H_
+
+#include <QObject>
+#include <QSqlDatabase>
+#include <QDateTime>
+
+#include <UniqLogger.h>
+
+class Logger;
+
+
+class DbhConfig
+{
+
+public:
+    //DB Stuff
+    QString dbType, dbConnectionName, dbTimeZone;
+    QString dbUser, dbPass, dbName, dbHost;
+    int dbPort;
+
+    //Logging stuff
+    QString logId, logFilename;
+    WriterConfig logwriterConfig;
+    UNQL::LogMessagePriorityType logLevel;
+
+    explicit DbhConfig(const QString &i_dbUser, const QString &i_dbPass, const QString &i_dbHost, const QString &i_dbName)
+        : dbType("QPSQL")
+        , dbConnectionName(QLatin1String( QSqlDatabase::defaultConnection ) )
+        , dbUser(i_dbUser)
+        , dbPass(i_dbPass)
+        , dbName(i_dbName)
+        , dbHost(i_dbHost)
+        , dbPort(5432)
+        , logId("NRDBH")
+        , logFilename("NRDBH.log")
+        , logLevel(UNQL::LOG_INFO)
+    {
+
+    }
+};
+
+
+class NrBaseDbHandler : public QObject
+{
+    Q_OBJECT
+
+protected:
+    DbhConfig m_DbConf;
+    QSqlDatabase _M_db;
+    Logger *m_logger;
+
+    bool openDbConn();
+    void closeDbConn();
+    bool executeQuery(QSqlQuery &q);
+    void rollbackAndClose(const QString &notice="");
+    void commitAndClose(const QString &notice="");
+
+public:
+    explicit NrBaseDbHandler(const DbhConfig& dbconf, QObject *parent = 0);
+
+    virtual ~NrBaseDbHandler()
+    {
+        QSqlDatabase::removeDatabase(m_DbConf.dbConnectionName);
+    }
+
+    bool testConnection();
+
+    inline const QString dbHost() const { return m_DbConf.dbHost; }
+    inline const QString dbName() const { return m_DbConf.dbName; }
+    inline const QString dbUsername() const { return m_DbConf.dbUser; }
+    inline const QString dbPassword() const { return m_DbConf.dbPass; }
+    inline const QString dbTimezone() const { return m_DbConf.dbTimeZone; }
+    inline const QString connectionName() const { return m_DbConf.dbConnectionName; }
+    inline int dbPort() const { return m_DbConf.dbPort; }
+};
+
+#endif // DBHANDLER_H
